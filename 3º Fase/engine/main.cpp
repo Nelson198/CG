58f1acf8d70ccae0930f26d185cdd9efd3075e67
JRI98 <38755101+JRI98@users.noreply.github.com>
2019-03-27 13:31:27 +0000

    Início da 3ª Fase

---
+++ 3º Fase/engine/main.cpp
@@ -0,0 +1,383 @@
+#include <stdlib.h>
+
+#ifdef __APPLE__
+#include <GLUT/glut.h>
+#else
+#include <GL/glut.h>
+#endif
+
+#define _USE_MATH_DEFINES
+#include <math.h>
+
+#include <iostream>
+#include <fstream>
+#include <vector>
+
+#include "tinyxml2.h"
+
+typedef struct {
+	float x;
+	float y;
+	float z;
+} Vertex;
+typedef std::vector<Vertex> Vertices;
+
+typedef struct {
+	char type;  // It can be: T - Translate, R - Rotate, S - Scale, C - Color
+	float param1; // These parameters depend on the type
+	float param2;
+	float param3;
+	float param4;
+} Transformation;
+typedef std::vector<Transformation> Transformations;
+
+typedef struct group {
+	Transformations trans;
+	Vertices vert;
+	std::vector<struct group> subGroups;
+} Group;
+
+Group mainGroup;
+
+GLdouble dist = 50, beta = M_PI_4, alpha = M_PI_4, xd = 0, zd = 0;
+
+// Function that handles the drawing of a given group
+void drawGroup(Group g) {
+	glPushMatrix();
+
+	float R, G, B;
+	bool changedColor = false;
+	for (Transformation t: g.trans) {
+		switch (t.type) {
+			case 'T':
+				glTranslatef(t.param1, t.param2, t.param3);
+				break;
+
+			case 'R':
+				glRotatef(t.param1, t.param2, t.param3, t.param4);
+				break;
+
+			case 'S':
+				glScalef(t.param1, t.param2, t.param3);
+				break;
+
+			case 'C':
+				if (t.param1 != 0 || t.param2 != 0 || t.param3 != 0) {
+					R = t.param1;
+					G = t.param2;
+					B = t.param3;
+					changedColor = true;
+				}
+				break;
+
+			default:
+				break;
+		}
+	}
+
+	glBegin(GL_TRIANGLES);
+	for (Vertex v: g.vert) {
+		if (!changedColor) {
+			// Give a different color to every vertex, so that a gradient effect is applied
+			glColor3f(rand() / (float) RAND_MAX, rand() / (float) RAND_MAX, rand() / (float) RAND_MAX);
+		} else {
+			float variation = (rand() / (float) RAND_MAX) / 5;
+			glColor3f(R + variation, G + variation, B + variation);
+		}
+		glVertex3f(v.x, v.y, v.z);
+	}
+	glEnd();
+
+	for (Group g: g.subGroups)
+		drawGroup(g);
+
+	glPopMatrix();
+}
+
+// Function that draws the axis
+void drawAxis() {
+	glBegin(GL_LINES);
+		// x
+		glColor3f(1.0, 0.0, 0.0);
+		glVertex3f(-100.0, 0.0, 0.0);
+		glVertex3f(100.0, 0.0, 0.0);
+		// y
+		glColor3f(0.0, 1.0, 0.0);
+		glVertex3f(0.0, -100.0, 0.0);
+		glVertex3f(0.0, 100.0, 0.0);
+		// z
+		glColor3f(0.0, 0.0, 1.0);
+		glVertex3f(0.0, 0.0, -100.0);
+		glVertex3f(0.0, 0.0, 100.0);
+	glEnd();
+}
+
+// Function called when it is necessary to render the scene
+void renderScene() {
+	// Clear buffers
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	// Set the camera
+	glLoadIdentity();
+	gluLookAt(dist*cos(beta)*sin(alpha), dist*sin(beta), dist*cos(beta)*cos(alpha),
+		      0.0, 0.0, 0.0,
+		      0.0f, 1.0f, 0.0f);
+
+	// Translate the vertices to the desired location
+	glTranslatef(xd, 0, zd);
+
+	// Draw the axis
+	// drawAxis();
+
+	// Draw the scene that was loaded from the XML file
+	drawGroup(mainGroup);
+
+	// End of frame
+	glutSwapBuffers();
+}
+
+// Function called when an ascii key is pressed
+void processKeys(unsigned char c, int xx, int yy) {
+	float deltaToZoom = 0.3;
+	float deltaToMove = 0.1;
+	switch (c) {
+		// Move the vertices in the (-deltaToMove, 0, -deltaToMove) direction
+		case 'w':
+			xd -= deltaToMove;
+			zd -= deltaToMove;
+			break;
+
+		// Move the vertices in the (-deltaToMove, 0, deltaToMove) direction
+		case 'a':
+			xd -= deltaToMove;
+			zd += deltaToMove;
+			break;
+
+		// Move the vertices in the (deltaToMove, 0, deltaToMove) direction
+		case 's':
+			xd += deltaToMove;
+			zd += deltaToMove;
+			break;
+
+		// Move the vertices in the (deltaToMove, 0, -deltaToMove) direction
+		case 'd':
+			xd += deltaToMove;
+			zd -= deltaToMove;
+			break;
+
+		// Move the camera closer to (0, 0, 0)
+		case 'q':
+			dist += deltaToZoom;
+			break;
+
+		// Move the camera further from (0, 0, 0)
+		case 'e':
+			dist -= deltaToZoom;
+			break;
+
+		// Show triangles as filled shapes
+		case 'f':
+			glPolygonMode(GL_FRONT, GL_FILL);
+			break;
+
+		// Show triangles' edges 
+		case 'l':
+			glPolygonMode(GL_FRONT, GL_LINE);
+			break;
+
+		// Show triangles' vertices
+		case 'p':
+			glPolygonMode(GL_FRONT, GL_POINT);
+			break;
+
+		default:
+			return;
+	}
+
+	glutPostRedisplay();
+}
+
+// Function called when a non-ascii key is pressed
+void processSpecialKeys(int key, int xx, int yy) {
+	float deltaToMove = 0.1;
+	switch (key) {
+		// Move the camera up
+		case GLUT_KEY_UP:
+			beta += deltaToMove;
+			break;
+
+		// Move the camera down
+		case GLUT_KEY_DOWN:
+			beta -= deltaToMove;
+			break;
+
+		// Move the camera to the left
+		case GLUT_KEY_LEFT:
+			alpha -= deltaToMove;
+			break;
+
+		// Move the camera to the right
+		case GLUT_KEY_RIGHT:
+			alpha += deltaToMove;
+			break;
+
+		default:
+			return;
+	}
+
+	glutPostRedisplay();
+}
+
+// Function called when the window is resized
+void resizeWindow(int w, int h) {
+	// Prevent a divide by zero, when window is too short
+	// (you cant make a window with zero width).
+	if (h == 0)
+		h = 1;
+
+	// Compute the window's aspect ratio 
+	float ratio = w * 1.0 / h;
+
+	// Set the projection matrix as current
+	glMatrixMode(GL_PROJECTION);
+
+	// Load Identity Matrix
+	glLoadIdentity();
+
+	// Set the viewport to be the entire window
+	glViewport(0, 0, w, h);
+
+	// Set the perspective
+	gluPerspective(45.0f, ratio, 1.0f, 1000.0f);
+
+	// Return to the model view matrix mode
+	glMatrixMode(GL_MODELVIEW);
+}
+
+// Function that creates a vertex object from its string representation "x y z"
+Vertex extractVertice(std::string s) {
+	std::string delimiter = " ";
+	float x, y, z;
+	int pos;
+	std::string token;
+
+	// Process the x coordinate
+	pos = s.find(delimiter);
+	token = s.substr(0, pos);
+	x = atof(token.c_str());
+	s.erase(0, pos + delimiter.length());
+
+	// Process the y coordinate
+	pos = s.find(delimiter);
+	token = s.substr(0, pos);
+	y = atof(token.c_str());
+	s.erase(0, pos + delimiter.length());
+
+	// Process the z coordinate
+	pos = s.find(delimiter);
+	token = s.substr(0, pos);
+	z = atof(token.c_str());
+
+	return Vertex{x,y,z};
+}
+
+// Function that processes and adds the vertices from a file to the allVertices vector
+void addVertices(std::ifstream &vertices, Group *group) {
+    char v[100];
+    while (vertices.getline(v, 100))
+        group->vert.push_back(extractVertice(v));
+}
+
+// Function that processes a group's information and returns an object with that info
+Group processGroup(tinyxml2::XMLElement *group) {
+	Group currentG;
+	// Add all the attributes inside the current group to its "group" object
+	for (tinyxml2::XMLElement *elem = group->FirstChildElement(); elem != nullptr; elem = elem->NextSiblingElement()) {
+		std::string elemName = elem->Value();
+		if (elemName == "translate") {
+			Transformation t = {type: 'T', param1: elem->FloatAttribute("X"), param2: elem->FloatAttribute("Y"), param3: elem->FloatAttribute("Z")};
+			currentG.trans.push_back(t);
+		} else if (elemName == "rotate") {
+			Transformation t = {type: 'R', param1: elem->FloatAttribute("angle"), param2: elem->FloatAttribute("axisX"), param3: elem->FloatAttribute("axisY"), param4: elem->FloatAttribute("axisZ")};
+			currentG.trans.push_back(t);			
+		} else if (elemName == "scale") {
+			Transformation t = {type: 'S', param1: elem->FloatAttribute("X"), param2: elem->FloatAttribute("Y"), param3: elem->FloatAttribute("Z")};
+			currentG.trans.push_back(t);
+		} else if (elemName == "models") {
+			// Iterate over all the models and load their vertices (including their base color)
+			for (tinyxml2::XMLElement *model = elem->FirstChildElement("model"); model != nullptr; model = model->NextSiblingElement("model")) {
+				// Extract the vertices
+				std::ifstream myfile;
+				myfile.open(model->Attribute("file"));
+				addVertices(myfile, &currentG);
+				myfile.close();
+
+				// Extract the color of the vetices
+				Transformation t = {type: 'C', param1: model->FloatAttribute("R"), param2: model->FloatAttribute("G"), param3: model->FloatAttribute("B")};
+				currentG.trans.push_back(t);
+			}
+		} else if (elemName == "group") {
+			Group child = processGroup(elem);
+			currentG.subGroups.push_back(child);
+		}
+	}
+
+	return currentG;
+}
+
+// Function that processes the XML file received as an argument
+void processXML(char **argv) {
+	// Load the XML file to memory
+	tinyxml2::XMLDocument doc;
+	tinyxml2::XMLError e = doc.LoadFile(argv[1]);
+	if (e != tinyxml2::XML_SUCCESS) {
+		std::cout << "Por favor, forneça um ficheiro XML válido\n";
+		exit(EXIT_FAILURE);
+	}
+
+	// Get the structure associated to the attribute "scene"
+	tinyxml2::XMLElement *scene = doc.FirstChildElement("scene");
+	if (!scene) {
+		std::cout << "Formato do ficheiro inválido\n";
+		exit(EXIT_FAILURE);
+	}
+
+	// Process the scene element and all its children
+	mainGroup = processGroup(scene);
+}
+
+int main(int argc, char **argv) {
+	if (argc != 2) {
+		std::cout << "Por favor, forneça um ficheiro XML\n";
+		exit(EXIT_FAILURE);
+	}
+
+	// Processing XML file
+	processXML(argv);
+
+	// Init GLUT and the window
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGBA);
+	glutInitWindowPosition(100, 100);
+	glutInitWindowSize(800, 800);
+	glutCreateWindow("Fase2TP - Engine");
+
+	// Register the required callback 
+	glutDisplayFunc(renderScene);
+	glutReshapeFunc(resizeWindow);
+
+	// Resgister callback for keyboard processing
+	glutKeyboardFunc(processKeys);
+	glutSpecialFunc(processSpecialKeys);
+
+	// Set OpenGL settings
+	glEnable(GL_DEPTH_TEST);
+	glEnable(GL_CULL_FACE);
+
+	glPointSize(3);
+
+	// Enter GLUT's main cycle
+	glutMainLoop();
+
+	return 1;
+}
---
+++ 3º Fase/generator/main.cpp
@@ -0,0 +1,250 @@
+#include <stdlib.h>
+#include <iostream>
+#include <fstream>
+
+#define _USE_MATH_DEFINES
+#include <math.h>
+
+std::ofstream outFile;
+
+// Function that prints a vertex's coordinates to outFile as "x y z\n"
+void printVertex(float x, float y, float z) {
+	outFile << x << " " << y << " " << z << "\n";
+}
+
+// Function that prints a plane's vertices to outFile
+void printPlane(int dist) {
+	printVertex(dist/2.0, 0, 0);
+	printVertex(0, 0, -dist/2.0);
+	printVertex(-dist/2.0, 0, 0);
+
+	printVertex(-dist/2.0, 0, 0);
+	printVertex(0, 0, dist/2.0);
+	printVertex(dist/2.0, 0, 0);
+
+	printVertex(dist/2.0, 0, 0);
+	printVertex(0, 0, dist/2.0);
+	printVertex(-dist/2.0, 0, 0);
+
+	printVertex(-dist/2.0, 0, 0);
+	printVertex(0, 0, -dist/2.0);
+	printVertex(dist/2.0, 0, 0);
+}
+
+// Function that prints a box's vertices to outFile
+void printBox(int dimX, int dimY, int dimZ, int divisions) {
+	for (int i = 0; i < divisions; i++) {
+		for (int j = 0; j < divisions; j++) {
+			double xOffset, yOffset, zOffset, nextXOffset, nextYOffset, nextZOffset;;
+
+			// Top triangles
+			// (x, +y, z)
+			xOffset = dimX/2.0 - i*dimX/(float)divisions, yOffset = dimY/2.0, zOffset = dimZ/2.0 - j*dimZ/(float)divisions;
+			nextXOffset = xOffset - dimX/(float)divisions, nextZOffset = zOffset - dimZ/(float)divisions;
+			printVertex(xOffset, yOffset, zOffset);
+			printVertex(xOffset, yOffset, nextZOffset);
+			printVertex(nextXOffset, yOffset, nextZOffset);
+
+			printVertex(nextXOffset, yOffset, nextZOffset);
+			printVertex(nextXOffset, yOffset, zOffset);
+			printVertex(xOffset, yOffset, zOffset);
+
+			// Lateral triangles
+			// (+x, y, z)
+			xOffset = dimX/2.0, yOffset = dimY/2.0 - i*dimY/(float)divisions, zOffset = dimZ/2.0 - j*dimZ/(float)divisions;
+			nextYOffset = yOffset - dimY/(float)divisions, nextZOffset = zOffset - dimZ/(float)divisions;
+			printVertex(xOffset, yOffset, zOffset);
+			printVertex(xOffset, nextYOffset, zOffset);
+			printVertex(xOffset, nextYOffset, nextZOffset);
+
+			printVertex(xOffset, yOffset, zOffset);
+			printVertex(xOffset, nextYOffset, nextZOffset);
+			printVertex(xOffset, yOffset, nextZOffset);
+
+			// (x, y, -z)
+			xOffset = dimX/2.0 - j*dimX/(float)divisions, yOffset = dimY/2.0 - i*dimY/(float)divisions, zOffset = -dimZ/2.0;
+			nextXOffset = xOffset - dimX/(float)divisions, nextYOffset = yOffset - dimY/(float)divisions;
+			printVertex(xOffset, yOffset, zOffset);
+			printVertex(xOffset, nextYOffset, zOffset);
+			printVertex(nextXOffset, nextYOffset, zOffset);
+
+			printVertex(xOffset, yOffset, zOffset);
+			printVertex(nextXOffset, nextYOffset, zOffset);
+			printVertex(nextXOffset, yOffset, zOffset);
+
+			// (-x, y, z)
+			xOffset = -dimX/2.0, yOffset = dimY/2.0 - i*dimY/(float)divisions, zOffset = -dimZ/2.0 + j*dimZ/(float)divisions;
+			nextYOffset = yOffset - dimY/(float)divisions, nextZOffset = zOffset + dimZ/(float)divisions;
+			printVertex(xOffset, yOffset, zOffset);
+			printVertex(xOffset, nextYOffset, zOffset);
+			printVertex(xOffset, nextYOffset, nextZOffset);
+
+			printVertex(xOffset, yOffset, zOffset);
+			printVertex(xOffset, nextYOffset, nextZOffset);
+			printVertex(xOffset, yOffset, nextZOffset);
+
+			// (x, y, +z)
+			xOffset = -dimX/2.0 + j*dimX/(float)divisions, yOffset = dimY/2.0 - i*dimY/(float)divisions, zOffset = dimZ/2.0;
+			nextXOffset = xOffset + dimX/(float)divisions, nextYOffset = yOffset - dimY/(float)divisions;
+			printVertex(xOffset, yOffset, zOffset);
+			printVertex(xOffset, nextYOffset, zOffset);
+			printVertex(nextXOffset, nextYOffset, zOffset);
+
+			printVertex(xOffset, yOffset, zOffset);
+			printVertex(nextXOffset, nextYOffset, zOffset);
+			printVertex(nextXOffset, yOffset, zOffset);
+
+			// Bottom triangles
+			// (x, -y, z)
+			xOffset = dimX/2.0 - i*dimX/(float)divisions, yOffset = -dimY/2.0, zOffset = dimZ/2.0 - j*dimZ/(float)divisions;
+			nextXOffset = xOffset - dimX/(float)divisions, nextZOffset = zOffset - dimZ/(float)divisions;
+			printVertex(xOffset, yOffset, zOffset);
+			printVertex(nextXOffset, yOffset, nextZOffset);
+			printVertex(xOffset, yOffset, nextZOffset);
+
+			printVertex(nextXOffset, yOffset, nextZOffset);
+			printVertex(xOffset, yOffset, zOffset);
+			printVertex(nextXOffset, yOffset, zOffset);
+		}
+	}
+}
+
+// Function that prints a sphere's vertices to outFile
+void printSphere(int radius, int slices, int stacks) {
+	// Angle of a single slice
+	double sliceDelta = 2 * M_PI / slices;
+	// Angle of a single stack
+	double betaDelta = M_PI_2 / (stacks / 2);
+
+	float r = radius;
+	// Generate the lower and upper stacks at the same time
+	for (int j = 0; j < ((float)stacks)/2; j++) {
+		float nextR = radius*cos(betaDelta*(j+1));
+		float height = radius*sin(betaDelta*(j));
+		float nextHeight = radius*sin(betaDelta*(j+1));
+
+		// The 0.5 adjustement in the for loop is to alternate between the triangles of
+		// adjacent stacks, so that they align correctly with each other
+		for (float i = 0.5*(j%2); i < slices + 0.5*(j%2); i++) {
+			// Triangles with the tip pointing up of the upper dome
+			printVertex(r*sin(i*sliceDelta), height, r*cos(i*sliceDelta));
+			printVertex(r*sin((i + 1)*sliceDelta), height, r*cos((i + 1)*sliceDelta));
+			printVertex(nextR*sin((i + 0.5)*sliceDelta), nextHeight, nextR*cos((i + 0.5)*sliceDelta));
+
+			// Triangles with the tip pointing up of the lower dome
+			printVertex(r*sin(i*sliceDelta), -height, r*cos(i*sliceDelta));
+			printVertex(nextR*sin((i + 0.5)*sliceDelta), -nextHeight, nextR*cos((i + 0.5)*sliceDelta));
+			printVertex(r*sin((i + 1)*sliceDelta), -height, r*cos((i + 1)*sliceDelta));
+
+
+			// This 0.5 adjustement (and the one below) is to offset the triangles in the same
+			// stack, so that they don't end up on top of each other
+			i += 0.5;
+
+			// Triangles with the tip pointing down of the upper dome
+			printVertex(nextR*sin((i + 1)*sliceDelta), nextHeight, nextR*cos((i + 1)*sliceDelta));
+			printVertex(nextR*sin(i*sliceDelta), nextHeight, nextR*cos(i*sliceDelta));
+			printVertex(r*sin((i + 0.5)*sliceDelta), height, r*cos((i + 0.5)*sliceDelta));
+
+			// Triangles with the tip pointing up of the lower dome
+			printVertex(nextR*sin((i + 1)*sliceDelta), -nextHeight, nextR*cos((i + 1)*sliceDelta));
+			printVertex(r*sin((i + 0.5)*sliceDelta), -height, r*cos((i + 0.5)*sliceDelta));
+			printVertex(nextR*sin(i*sliceDelta), -nextHeight, nextR*cos(i*sliceDelta));
+
+			i -= 0.5;
+		}
+		r = nextR;
+	}
+}
+
+// Function that prints a cone's vertices to outFile
+void printCone(int bottomRadius, int height, int slices, int stacks) {
+	// Angle of a single slice
+	double sliceDelta = 2 * M_PI / slices;
+
+	float r = bottomRadius;
+	// Draw the bottom circle
+	for (float i = 0; i < slices; i++) {
+		printVertex(0, 0, 0);
+		printVertex(r*sin((i+1)*sliceDelta), 0, r*cos((i+1)*sliceDelta));
+		printVertex(r*sin(i*sliceDelta), 0, r*cos(i*sliceDelta));
+	}
+
+	float rnext = r - (bottomRadius/((float)stacks));
+	// Draw each stack, one at a time
+	for(int j = 0; j < stacks; j++) {
+		float h = j*(((float)height)/stacks);
+		float hnext = (j+1)*(((float)height)/stacks);
+
+		// The 0.5 adjustement in the for loop is to alternate between the triangles of
+		// adjacent stacks, so that they align correctly with each other
+		for (float i = 0.5*(j%2); i < slices + 0.5*(j%2); i++) {
+			// Triangles with the tip pointing up
+			printVertex(r*sin(i*sliceDelta), h, r*cos(i*sliceDelta));
+			printVertex(r*sin((i + 1)*sliceDelta), h, r*cos((i + 1)*sliceDelta));
+			printVertex(rnext*sin((i + 0.5)*sliceDelta), hnext, rnext*cos((i + 0.5)*sliceDelta));
+
+			// This 0.5 adjustement (and the one below) is to offset the triangles in the same
+			// stack, so that they don't end up on top of each other
+			i += 0.5;
+
+			// Triangles with the tip pointing down
+			printVertex(rnext*sin((i + 1)*sliceDelta), hnext, rnext*cos((i + 1)*sliceDelta));
+			printVertex(rnext*sin(i*sliceDelta), hnext, rnext*cos(i*sliceDelta));
+			printVertex(r*sin((i + 0.5)*sliceDelta), h, r*cos((i + 0.5)*sliceDelta));
+			i -= 0.5;
+		}
+		r = rnext;
+		rnext -= (bottomRadius/((float)stacks));
+	}
+}
+
+int main(int argc, char **argv) {
+	if (argc < 4) {
+		std::cout << "Indique a forma geométrica que pretende gerar e o ficheiro de destino\n";
+		exit(EXIT_FAILURE);
+	}
+
+  	outFile.open(argv[argc-1]);
+	if (std::string(argv[1]) == "plane") {
+		if (argc != 4) {
+			std::cout << "Número de argumentos para o plano incorreto\n";
+			exit(EXIT_FAILURE);
+		}
+
+		int dist = atoi(argv[2]);
+		printPlane(dist);
+	} else if (std::string(argv[1]) == "box") {
+		if (argc != 7 && argc != 6) {
+			std::cout << "Número de argumentos para a caixa incorreto\n";
+			exit(EXIT_FAILURE);
+		}
+
+		int dimX = atoi(argv[2]), dimY = atoi(argv[3]), dimZ = atoi(argv[4]), divisions = 1;
+		if (argc == 7)
+			divisions = atoi(argv[5]);
+		printBox(dimX, dimY, dimZ, divisions);
+	} else if (std::string(argv[1]) == "sphere") {
+		if (argc != 6) {
+			std::cout << "Número de argumentos para a esfera incorreto\n";
+			exit(EXIT_FAILURE);
+		}
+
+		int radius = atoi(argv[2]), slices = atoi(argv[3]), stacks = atoi(argv[4]);
+		printSphere(radius, slices, stacks);
+	} else if (std::string(argv[1]) == "cone") {
+		if (argc != 7) {
+			std::cout << "Número de argumentos para o cone incorreto\n";
+			exit(EXIT_FAILURE);
+		}
+
+		int bottomRadius = atoi(argv[2]), height = atoi(argv[3]), slices = atoi(argv[4]), stacks = atoi(argv[5]);
+		printCone(bottomRadius, height, slices, stacks);
+	} else {
+		std::cout << "Figura geométrica não suportada\n";
+		exit(EXIT_FAILURE);
+	}
+  	outFile.close();
+
+	return 1;
+}

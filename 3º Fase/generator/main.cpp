71fa2553fcbfd4718e68737f23486bceb34d7358
Nelson198 <nteixeira88@gmail.com>
2019-04-15 11:56:21 +0100

    Generator: -> falta fazer a função bezierGenerator que irá gerar os triângulos para desenhar a superfície pretendida.

--- 3º Fase/generator/main.cpp
+++ 3º Fase/generator/main.cpp
@@ -201,9 +201,75 @@ void printCone(int bottomRadius, int height, int slices, int stacks) {
 	}
 }
 
+/* <------------------------------------------------ Bézier patches ------------------------------------------------> */
+
+/* Struct of control points */
+struct CP {
+	float x;
+	float y;
+	float z;
+};
+
+/* Compute the position of a point along a Bezier curve at t [0:1] */
+float bezierCurve(float P[3], float t) 
+{ 
+    float b0 = (1 - t) * (1 - t) * (1 - t); 
+    float b1 = 3 * t * (1 - t) * (1 - t); 
+    float b2 = 3 * t * t * (1 - t); 
+    float b3 = t * t * t;
+
+    return P[0] * b0 + P[1] * b1 + P[2] * b2 + P[3] * b3; 
+}
+
+float bezierDerivative(float P[3], float t) 
+{ 
+    return -3 * (1 - t) * (1 - t) * P[0] + 
+           (3 * (1 - t) * (1 - t) - 6 * t * (1 - t)) * P[1] + 
+           (6 * t * (1 - t) - 3 * t * t) * P[2] + 
+            3 * t * t * P[3]; 
+}
+ 
+float bezierPatch(float *controlPoints, float u, float v) 
+{ 
+    float uCurve[4]; 
+    for (int i = 0; i < 4; i++) {
+		uCurve[i] = bezierCurve(controlPoints + 4 * i, u); 
+	}
+	return bezierCurve(uCurve, v); 
+}
+
+/* Compute the derivative of a point on Bezier patch along the u parametric direction */
+float bezierDU(float *controlPoints, float u, float v) 
+{ 
+    float P[4]; 
+    float vCurve[4]; 
+    for (int i = 0; i < 4; ++i) { 
+        P[0] = controlPoints[i]; 
+        P[1] = controlPoints[4 + i]; 
+        P[2] = controlPoints[8 + i]; 
+        P[3] = controlPoints[12 + i]; 
+        vCurve[i] = bezierCurve(P, v); 
+    } 
+    return bezierDerivative(vCurve, u); 
+}
+
+/* Compute the derivative of a point on Bezier patch along the v parametric direction */
+float bezierDV(float *controlPoints, float u, float v) 
+{ 
+    float uCurve[4];
+    for (int i = 0; i < 4; ++i) { 
+        uCurve[i] = bezierCurve(controlPoints + 4 * i, u); 
+    } 
+    return bezierDerivative(uCurve, v); 
+}
+
+/* Generate a list of triangles to draw the Bézier surface */
+void bezierGenerate(std::vector<std::vector<int>> patches, std::vector<CP> controlPoints) {
+
+}
+
 void printBezier(char *filePatch, int tesselation) {
-	// Open a file with the same name (but ending in ".3d")
-	// to output the triangles to
+	// Open a file with the same name (but ending in ".3d") to output the triangles to
 	std::ifstream patchFile;
 	patchFile.open(filePatch);
 	char *fileOut = (char *) malloc(strlen(filePatch)-2);
@@ -239,13 +305,9 @@ void printBezier(char *filePatch, int tesselation) {
 	// Get the control points
 	patchFile.getline(line, 1000);
 	int numControlPoints = atoi(line);
-	struct CP {
-		float x;
-		float y;
-		float z;
-	};
 	std::vector<CP> controlPoints;
 	controlPoints.reserve(numControlPoints);
+
 	for (int i = 0; i < numControlPoints; i++) {
 		patchFile.getline(line, 1000);
 
@@ -275,8 +337,10 @@ void printBezier(char *filePatch, int tesselation) {
 		controlPoints.push_back(CP{x, y, z});
 	}
 
+	bezierGenerate(patches, controlPoints);
+
 	///// FALTA USAR A TESSELATION PARA A IMAGEM FICAR MELHOR /////
-	for (auto patch : patches) {
+	/*for (auto patch : patches) {
 		for (int i = 0; i < patch.size()-2; i++) {
 			CP aux = controlPoints.at(patch.at(i));
 			printVertex(aux.x, aux.y, aux.z);
@@ -287,7 +351,7 @@ void printBezier(char *filePatch, int tesselation) {
 			aux = controlPoints.at(patch.at(i+2));
 			printVertex(aux.x, aux.y, aux.z);
 		}
-	}
+	}*/
 
 	patchFile.close();
 	outFile.close();
